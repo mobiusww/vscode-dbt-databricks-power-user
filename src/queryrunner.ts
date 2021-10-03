1bbf7894b7a2dced402e16c33d1cc5477d065f94
Butch Landingin <butchland@gmail.com>
2021-10-03 19:14:49 +0800

    Add code for query runner

---
+++ src/queryrunner.ts
@@ -0,0 +1,254 @@
+import * as vscode from "vscode";
+import { BigQuery, Job, BigQueryOptions } from "@google-cloud/bigquery";
+import * as flatten from "flat";
+
+// import {GoogleAuth} from "./google_auth";
+
+// CommandMap describes a map of extension commands (defined in package.json)
+// and the function they invoke.
+// type CommandMap = Map<string, () => void>;
+// let commands: CommandMap = new Map<string, () => void>([
+//   ["extension.runAsQuery", runAsQuery],
+//   ["extension.runSelectedAsQuery", runSelectedAsQuery],
+//   ["extension.dryRun", dryRun]
+// ]);
+
+interface QueryResult {
+  status: "success";
+  sql?: string;
+  info: { [s: string]: any };
+  table: TableResult;
+  json: string;
+  detail: string;
+}
+
+interface QueryResultError {
+  status: "error";
+  errorMessage: string;
+}
+
+interface TableResult {
+  headers: string[];
+  rows: any[];
+}
+
+export class BigQueryRunner {
+  configPrefix = "queryRunner";
+  config: vscode.WorkspaceConfiguration;
+  client: BigQuery;
+  output = vscode.window.createOutputChannel("Query Runner");
+  job: Job | null = null;
+  editor: vscode.TextEditor;
+  //googleAuth: GoogleAuth;
+
+  constructor(config: vscode.WorkspaceConfiguration, editor: vscode.TextEditor) {
+    this.config = config;
+    this.editor = editor;
+    let options: BigQueryOptions = {
+      keyFilename: this.config?.get("keyFilename"),
+      projectId: this.config?.get("projectId"),
+    };
+    this.client = new BigQuery(options);
+  }
+
+  setConfig(config: vscode.WorkspaceConfiguration) {
+    this.config = config;
+  }
+
+  // getAuthorizeUrl () {
+  //   return this.googleAuth.getAuthorizeUrl();
+  // }
+
+  // async setRefreshClient (authCode: string) {
+  //   const refreshClient = await this.googleAuth.setRefreshClient(authCode);
+  //   this.client.authClient.cachedCredential = refreshClient;
+  // }
+
+  /**
+   * @param queryText
+   * @param isDryRun Defaults to False.
+   */
+  private async query(queryText: string, isDryRun?: boolean): Promise<QueryResult> {
+    let data;
+    console.log('queryrunner run query: queryText: ${queryText}');
+    try {
+      data = await this.client.createQueryJob({
+        query: queryText,
+        // location: config.get("location"),
+        // maximumBytesBilled: config.get("maximumBytesBilled"),
+        // useLegacySql: config.get("useLegacySql"),
+        dryRun: !!isDryRun
+      });
+    } catch (err) {
+      vscode.window.showErrorMessage(`Failed to query BigQuery: ${err}`);
+      throw err;
+    }
+    this.job = data[0];
+
+    if (!this.job) {
+      throw new Error("No job was found.");
+    }
+
+    // if (isDryRun) {
+    //   vscode.window.showInformationMessage(`${jobIdMessage} (dry run)`);
+    //   let totalBytesProcessed = job.metadata.statistics.totalBytesProcessed;
+    //   writeDryRunSummary(id, totalBytesProcessed);
+    //   return null;
+    // }
+
+    vscode.window.showInformationMessage(`BigQuery job ID: ${this.job.metadata.id}`);
+
+    let result;
+
+    try {
+      result = await this.job.getQueryResults({
+        autoPaginate: true // TODO
+      });
+    } catch (err) {
+      vscode.window.showErrorMessage(`Failed to query BigQuery: ${err}`);
+      throw err;
+    }
+
+    try {
+      return await this.processResults(result[0]);
+    } catch (err) {
+      vscode.window.showErrorMessage(`Failed to get results: ${err}`);
+      throw err;
+    }
+  }
+
+  private makeTable(rows: Array<any>): TableResult {
+    const headers: string[] = [];
+    Object.keys(flatten(rows[0], { safe: true })).forEach(name => headers.push(name));
+
+    let table: any[] = [];
+
+    rows.forEach((val, idx) => {
+      // Flatten each row, and for each header (name), insert the matching
+      // object property (v[name])
+      let v: { [s: string]: any } = flatten(val, { safe: true });
+      let tableRow: any[] = [];
+      headers.forEach((name, col) => {
+        tableRow.push(v[name]);
+      });
+      table.push(tableRow);
+    });
+
+    return {
+      headers,
+      rows: table
+    };
+  }
+
+  private async processResults(rows: Array<any>): Promise<QueryResult> {
+    if (!this.job) {
+      throw new Error('No job was found.');
+    }
+
+    const metadata = (await this.job.getMetadata())[0];
+
+    return {
+      status: "success",
+      info: {
+        projectId: metadata.jobReference.projectId,
+        jobId: metadata.id,
+        location: this.job.location,
+        jobLink: metadata.selfLink,
+        creationTime: metadata.statistics.creationTime,
+        startTime: metadata.statistics.startTime,
+        endTime: metadata.statistics.endTime,
+        userEmail: metadata.user_email,
+        totalBytesProcessed: metadata.statistics.totalBytesProcessed,
+        status: metadata.status.state,
+      },
+      table: this.makeTable(rows),
+      json: JSON.stringify(rows, null, "  "),
+      detail: JSON.stringify(metadata.statistics, null, "  "),
+    };
+  }
+
+  // private writeDryRunSummary(jobId: string, numBytesProcessed: string) {
+  //   this.output.show();
+  //   this.output.appendLine(`Results for job ${jobId} (dry run):`);
+  //   this.output.appendLine(`Total bytes processed: ${numBytesProcessed}`);
+  //   this.output.appendLine(``);
+  // }
+
+  public async runAsQuery(variables: { [s: string]: any }, onlySelected?: boolean): Promise<QueryResult | QueryResultError> {
+    try {
+      const queryText = this.getQueryText(variables, onlySelected);
+      console.log('BigQueryRunner.runAsQuery:  ${queryText}');
+      let queryResult = await this.query(queryText);
+      queryResult.sql = queryText;
+      return queryResult;
+    } catch (err) {
+      vscode.window.showErrorMessage('$err');
+      return {
+        status: "error",
+        errorMessage: (err instanceof Error)? err.message:'',
+      };
+    }
+  }
+
+  public async cancelQuery(): Promise<any> {
+    if (!this.job) {
+      vscode.window.showErrorMessage('No job was found.');
+      return;
+    }
+
+    const result = await this.job.cancel();
+    return result;
+  }
+
+  private getQueryText(variables: { [s: string]: any }, onlySelected?: boolean): string {
+    if (!this.editor) {
+      throw new Error("No active editor window was found");
+    }
+
+    let text: string;
+
+    // Only return the selected text
+    if (onlySelected) {
+      const selection = this.editor.selection;
+      if (selection.isEmpty) {
+        throw new Error("No text is currently selected");
+      }
+
+      text = this.editor.document.getText(selection).trim();
+    } else {
+      text = this.editor.document.getText().trim();
+    }
+
+    if (!text) {
+      throw new Error("The editor window is empty");
+    }
+
+    // Replace variables
+    for (let [key, value] of Object.entries(variables)) {
+      const re = new RegExp(key, 'g');
+      text = text.replace(re, value);
+    }
+
+    return text;
+  }
+
+  // function runSelectedAsQuery(): void {
+  //   try {
+  //     let queryText = getQueryText(vscode.window.activeTextEditor, true);
+  //     query(queryText);
+  //   } catch (err) {
+  //     vscode.window.showErrorMessage(err);
+  //   }
+  // }
+
+  // function dryRun(): void {
+  //   try {
+  //     let queryText = getQueryText(vscode.window.activeTextEditor);
+  //     query(queryText, true);
+  //   } catch(err) {
+  //     vscode.window.showErrorMessage(err);
+  //   }
+  // }
+
+}
+
---
+++ src/queryrunner_install.ts
@@ -0,0 +1,127 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { BigQueryRunner } from './queryrunner';
+
+const configPrefix = "bigquery";
+let config: vscode.WorkspaceConfiguration;
+// let output = vscode.window.createOutputChannel("QueryRunner");
+async function openQueryRunner(context: vscode.ExtensionContext, 
+								 bigQueryRunner: BigQueryRunner) {
+	let localResourceRoot = vscode.Uri.file(path.join(context.extensionPath,'public'));
+	console.log('localResourceRoot: ' + localResourceRoot);
+	const panel = vscode.window.createWebviewPanel(
+		'queryRunner', // Identifies the type of the webview. Used internally
+		'QueryRunner', // Title of the panel displayed to the user
+		vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+		{
+			enableScripts: true,
+			retainContextWhenHidden: true,
+			localResourceRoots: [localResourceRoot]
+		}
+	);
+
+	panel.webview.html = getWebviewContent(context);
+
+	// Send variables to webview.
+	const variables = context.workspaceState.get("variables", {});
+	panel.webview.postMessage({ command: 'setVariables', variables });
+
+	// Set authorize URL.
+	// const authorizeUrl = bigQueryRunner.getAuthorizeUrl();
+	// panel.webview.postMessage({ command: 'setAuthorizeUrl', authorizeUrl });
+
+	panel.webview.onDidReceiveMessage(
+		async message => {
+			switch (message.command) {
+				case 'openExternal':
+						vscode.env.openExternal(message.url);
+					break;
+
+				// case 'setAuthCode':
+				// 		const r = await bigQueryRunner.setRefreshClient(message.authCode);
+				// 		panel.webview.postMessage({ command: 'authSuccessed' });
+				// 	break;
+
+				case 'runAsQuery':
+					const queryResult = await bigQueryRunner.runAsQuery(message.variables, message.onlySelected);
+					if (queryResult.status === "error") {
+						panel.webview.postMessage({ command: 'queryError', errorMessage: queryResult.errorMessage });
+					} else {
+						panel.webview.postMessage({ command: 'runAsQuery', result: queryResult });
+					}
+					break;
+
+				case 'cancelQuery':
+					const cancelResult = await bigQueryRunner.cancelQuery();
+					panel.webview.postMessage({ command: 'cancelQuery', result: cancelResult });
+					break;
+
+				case 'saveVariables':
+					context.workspaceState.update("variables", message.variables);
+					break;
+			}
+		},
+		undefined,
+		context.subscriptions
+	);
+}
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	readConfig();
+
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+
+	const bigQueryRunner = new BigQueryRunner(config, editor);
+
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeConfiguration(event => {
+			if (!event.affectsConfiguration(configPrefix)) {
+				return;
+			}
+
+			readConfig();
+			bigQueryRunner.setConfig(config);
+		})
+	);
+	let disposable = vscode.commands.registerCommand("dbtPowerUser.openQueryRunner", async() => {await openQueryRunner(context, bigQueryRunner);});
+	context.subscriptions.push(disposable);
+}
+
+function readConfig(): void {
+	try {
+		config = vscode.workspace.getConfiguration(configPrefix);
+	} catch (e) {
+		vscode.window.showErrorMessage(`failed to read config: ${e}`);
+	}
+}
+
+function getWebviewContent(context: vscode.ExtensionContext): string {
+	let indexPath = vscode.Uri.file(
+		path.join(context.extensionPath, 'public', 'index.html')
+	);
+	indexPath = indexPath.with({ scheme: 'vscode-resource' });
+
+	let html = fs.readFileSync(indexPath.path).toString();
+
+	let resourceDir = vscode.Uri.file(
+		path.join(context.extensionPath, 'public')
+	);
+	resourceDir = resourceDir.with({ scheme: 'vscode-resource' });
+
+	return html.replace(new RegExp('__RESOURCE_DIR__', 'g'), resourceDir.toString());
+}
+
+// function publicPath(filePath: string, context: vscode.ExtensionContext): string {
+// }
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
