d00e73e6dd1b34670ace72750678bac86b4ae00c
Butch Landingin <butchland@gmail.com>
2021-10-31 14:12:31 +0800

    Polish format changes

--- public/main.js
+++ public/main.js
@@ -1,150 +1,151 @@
 const vscode = acquireVsCodeApi();
 
-async function call (param) {
-  return await vscode.postMessage (param);
+async function call(param) {
+    return await vscode.postMessage(param);
 }
 
-const vueApp = new Vue ({
-  el: '#app',
-  data: {
-    activeTag: 'info',
-    queryStatus: 'none',
-    sql: null,
-    variablesString: '{}',
-    info: null,
-    table: null,
-    json: '',
-    detail: 'det',
-    errorMessage: null,
-    startIndex: 0,
-    hasNext: false,
-    hasPrev: false,
-    theme: 'dark',
-  },
-  computed: {
-    elapsedTime () {
-      if (!this.info || !this.info.startTime || !this.info.endTime) {
-        return;
-      }
-
-      return (parseInt(this.info.endTime) - parseInt(this.info.startTime)) / 1000;
+const vueApp = new Vue({
+    el: '#app',
+    data: {
+        activeTag: 'info',
+        queryStatus: 'none',
+        sql: null,
+        variablesString: '{}',
+        info: null,
+        table: null,
+        json: '',
+        detail: 'det',
+        errorMessage: null,
+        startIndex: 0,
+        hasNext: false,
+        hasPrev: false,
+        theme: 'dark',
     },
-    totalBytesProcessed () {
-      if (!this.info || !this.info.totalBytesProcessed) {
-        return;
-      }
+    computed: {
+        elapsedTime() {
+            if (!this.info || !this.info.startTime || !this.info.endTime) {
+                return;
+            }
 
-      return this.info.totalBytesProcessed; // TODO
-    }
-  },
-  watch: {
-    variablesString (val) {
-      if (this._parseVariables()) {
-        call ({
-          command: 'saveVariables',
-          variables: this._parseVariables(),
-        });
-      }
-    }
-  },
-  methods: {
-    _parseVariables () {
-      try {
-        return JSON.parse(this.variablesString);
-      } catch {
-        return null;
-      }
+            return (parseInt(this.info.endTime) - parseInt(this.info.startTime)) / 1000;
+        },
+        totalBytesProcessed() {
+            if (!this.info || !this.info.totalBytesProcessed) {
+                return;
+            }
+
+            return this.info.totalBytesProcessed; // TODO
+        }
     },
-
-    nextPage () {
-      this.queryStatus = 'runningAsQuery';
-      call ({
-        command: 'nextPage',
-        variables: this._parseVariables() || {},
-      });
+    watch: {
+        variablesString(val) {
+            if (this._parseVariables()) {
+                call({
+                    command: 'saveVariables',
+                    variables: this._parseVariables(),
+                });
+            }
+        }
     },
-  
-    prevPage () {
-      this.queryStatus = 'runningAsQuery';
-      call ({
-        command: 'prevPage',
-        variables: this._parseVariables() || {},
-      });
+    methods: {
+        _parseVariables() {
+            try {
+                return JSON.parse(this.variablesString);
+            } catch {
+                return null;
+            }
+        },
+
+        nextPage() {
+            this.queryStatus = 'runningAsQuery';
+            call({
+                command: 'nextPage',
+                variables: this._parseVariables() || {},
+            });
+        },
+
+        prevPage() {
+            this.queryStatus = 'runningAsQuery';
+            call({
+                command: 'prevPage',
+                variables: this._parseVariables() || {},
+            });
+        },
+
+        runAsQuery() {
+            this.queryStatus = 'runningAsQuery';
+            call({
+                command: 'runAsQuery',
+                variables: this._parseVariables() || {},
+            });
+        },
+
+        displayResult(result) {
+            this.activeTag = 'table';
+            this.queryStatus = 'done';
+            this.sql = result.sql;
+            this.info = result.info;
+            this.table = result.table;
+            this.startIndex = result.startIndex;
+            this.theme = result.theme;
+            // this.json = result.json
+            this.detail = result.detail;
+            this.hasNext = result.hasNext ? "true" : "false";
+            this.hasPrev = result.hasPrev ? "true" : "false";
+        },
+
+        displayValue(value) {
+            if (value === null) {
+                return 'NULL';
+            } else {
+                return value;
+            }
+        },
+
+        cancelQuery() {
+            this.queryStatus = 'none';
+
+            call({
+                command: 'cancelQuery',
+            });
+        },
+
+        didCancelQuery() {
+            this.queryStatus = 'none';
+        },
+
+        displayError(errorMessage) {
+            this.queryStatus = 'error';
+            this.errorMessage = errorMessage;
+        },
+
+        setVariables(variables) {
+            this.variablesString = JSON.stringify(variables, null, "  ");
+        },
+
+
     },
-
-    runAsQuery () {
-      this.queryStatus = 'runningAsQuery';
-      call ({
-        command: 'runAsQuery',
-        variables: this._parseVariables() || {},
-      });
-    },
-
-    displayResult (result) {
-      this.activeTag = 'table';
-      this.queryStatus = 'done';
-      this.sql = result.sql;
-      this.info = result.info;
-      this.table = result.table;
-      this.startIndex = result.startIndex;
-      // this.json = result.json
-      this.detail = result.detail;
-      this.hasNext = result.hasNext? "true":"false";
-      this.hasPrev = result.hasPrev? "true":"false";
-    },
-
-    displayValue (value) {
-      if (value === null) {
-        return 'NULL';
-      } else {
-        return value;
-      }
-    },
-
-    cancelQuery () {
-      this.queryStatus = 'none';
-
-      call ({
-        command: 'cancelQuery',
-      });
-    },
-
-    didCancelQuery () {
-      this.queryStatus = 'none';
-    },
-
-    displayError (errorMessage) {
-      this.queryStatus = 'error';
-      this.errorMessage = errorMessage;
-    },
-
-    setVariables (variables) {
-      this.variablesString = JSON.stringify(variables, null, "  ");
-    },
-
-
-  },
 });
 
-window.addEventListener ('message', event => {
-  switch (event.data.command) {
-    case 'runAsQuery':
-      vueApp.displayResult(event.data.result);
-      break;
-    case 'nextPage':
-      vueApp.displayResult(event.data.result);
-      break;
-    case 'prevPage':
-      vueApp.displayResult(event.data.result);
-      break;
-    case 'queryError':
-      vueApp.displayError(event.data.errorMessage);
-      break;
-    case 'cancelQuery':
-      vueApp.didCancelQuery();
-      break;
-    case 'setVariables':
-      vueApp.setVariables(event.data.variables);
-      break;
-  }
+window.addEventListener('message', event => {
+    switch (event.data.command) {
+        case 'runAsQuery':
+            vueApp.displayResult(event.data.result);
+            break;
+        case 'nextPage':
+            vueApp.displayResult(event.data.result);
+            break;
+        case 'prevPage':
+            vueApp.displayResult(event.data.result);
+            break;
+        case 'queryError':
+            vueApp.displayError(event.data.errorMessage);
+            break;
+        case 'cancelQuery':
+            vueApp.didCancelQuery();
+            break;
+        case 'setVariables':
+            vueApp.setVariables(event.data.variables);
+            break;
+    }
 });
